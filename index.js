fee8c87e04d16d7ef7725a62d9cc72afaf903586
Neal JIn Oh <no4259@gmail.com>
2019-07-10 03:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var music = [];
+    for (var i = 0; i < musicians.length; i += 1) {
+        music.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return music;
+}
